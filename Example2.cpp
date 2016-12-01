696a2ded1c1cb0dfbe07662b9860b1f65623adf0
Aleksey Fedotov <0xc0dec@gmail.com>
2016-12-01 00:01:00 +0000

    Removed redundant virtuals, fixed formatting in shader code

--- Example2.cpp
+++ Example2.cpp
@@ -13,36 +13,36 @@ static struct
             #version 330 core
 
             in vec4 position;
-	    in vec2 texCoord0;
+	        in vec2 texCoord0;
 
             uniform mat4 worldMatrix;
             uniform mat4 viewProjMatrix;
-	    out vec2 uv0;
+	        out vec2 uv0;
 
             void main()
-	    {
-	        gl_Position = viewProjMatrix * worldMatrix * position;
-	        uv0 = texCoord0;
-	    }
+	        {
+	            gl_Position = viewProjMatrix * worldMatrix * position;
+	            uv0 = texCoord0;
+	        }
         )";
     } vertex;
 
     struct
     {
         const char* font = R"(
-	    #version 330 core
+	        #version 330 core
 
             uniform sampler2D mainTex;
 
             in vec2 uv0;
-	    out vec4 fragColor;
+	        out vec4 fragColor;
 
             void main()
-	    {
+	        {
                 vec4 c = texture(mainTex, uv0);
-	        fragColor = vec4(c.r, c.r, c.r, 1 - c.r);
-	    }
-	)";
+	            fragColor = vec4(c.r, c.r, c.r, 1 - c.r);
+	        }
+	    )";
     } fragment;
 } shaders;
 
@@ -62,9 +62,9 @@ private:
     void initTextQuad();
     void renderTextQuad();
 
-    virtual void init() override final;
-    virtual void shutdown() override final;
-    virtual void render(float dt) override final;
+    void init() override final;
+    void shutdown() override final;
+    void render(float dt) override final;
 
     struct
     {
