4a8e47bc021ca3c6b36368f4fd78a351a2348731
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2020-06-25 00:54:25 +0500

    Vulkan screen clearing works

--- demos/common/vk/VulkanAppBase.h
+++ demos/common/vk/VulkanAppBase.h
@@ -20,6 +20,8 @@ namespace vk
 	protected:
 		// TODO avoid casting
 		auto window() const -> vk::Window* { return dynamic_cast<Window*>(window_.get()); }
+        auto swapchain() -> Swapchain& { return swapchain_;  }
+        auto device() -> Device& { return device_; }
 
 	private:
         Device device_;
--- demos/vulkan/Main.cpp
+++ demos/vulkan/Main.cpp
@@ -4,6 +4,7 @@
 */
 
 #include "common/vk/VulkanAppBase.h"
+#include "common/vk/VulkanCmdBuffer.h"
 
 class App: public vk::AppBase
 {
@@ -13,13 +14,44 @@ public:
 	}
 
 protected:
+    vk::CmdBuffer cmdBuf_;
+    vk::Resource<VkSemaphore> completeSemaphore_;
+    VkSemaphore waitSemaphore_;
+
 	void init() override
 	{
+        cmdBuf_ = vk::CmdBuffer(device());
+        completeSemaphore_ = vk::createSemaphore(device());
 	}
 	
 	void render() override
 	{
-		
+        waitSemaphore_ = swapchain().moveNext();
+
+        const auto fb = swapchain().currentFrameBuffer();
+        auto &renderPass = swapchain().renderPass();
+        const auto canvasWidth = window()->canvasWidth();
+        const auto canvasHeight = window()->canvasHeight();
+
+        const VkClearRect clearRect{{{0, 0}, {static_cast<uint32_t>(canvasWidth), static_cast<uint32_t>(canvasHeight)}}, 0, 1};
+        const VkClearValue clearValue{{{1, 0, 0, 1}}};
+        const glm::vec4 viewport{0, 0, canvasWidth, canvasHeight};
+
+        cmdBuf_.begin(false)
+            .beginRenderPass(renderPass, fb, canvasWidth, canvasHeight)
+            .clearColorAttachment(0, clearValue, clearRect)
+            .setViewport(viewport, 0, 1)
+            .setScissor(viewport)
+	        .endRenderPass()
+	        .end();
+
+        vk::queueSubmit(device().queue(), 1, &waitSemaphore_, 1, &completeSemaphore_, 1, cmdBuf_);
+        vk::assertResult(vkQueueWaitIdle(device().queue()));
+
+        waitSemaphore_ = completeSemaphore_;
+
+        swapchain().present(device().queue(), 1, &waitSemaphore_);
+        vk::assertResult(vkQueueWaitIdle(device().queue()));
 	}
 	
 	void cleanup() override
